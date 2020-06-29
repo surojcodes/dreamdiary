cb9bdd72733e75b4dc6bdad7a96270992d108979
Suroj Maharjan <surojcodes@gmail.com>
2020-06-29 20:29:43 +0545

    Initial server setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+
+// configue to use env vars
+dotenv.config({ path: './config/config.env' });
+
+
+//Register Route 
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to Dream Diary</h1>');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
